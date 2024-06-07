578a94e34bb0ec7bbff743ba94f5debd0aa3b668
ktraieh <158105841+ktraieh@users.noreply.github.com>
2024-06-06 21:55:16 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
-import sys 
-  
+import sys
+import re
+
 for line in sys.stdin: 
-    line = line.strip() 
-    words = line.split() 
+    words = re.findall(r'[a-zA-Z]+', line) 
     for word in words:
         print('%s\t%s' % (word, 1))
